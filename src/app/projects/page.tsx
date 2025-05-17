d3d25b99545f869e10ff974e55508bfcf8f59360
Bas77 <sebastianramli@rocketmail.com>
2025-05-17 15:37:59 +0700

    Testing with 3d models

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import Head from 'next/head'
+
+function MiyuModel() {
+  const { scene } = useGLTF('/model/miyu/scene.gltf')
+  return <primitive object={scene} scale={0.5} />
+}
+
+export default function MiyuPage() {
+  return (
+    <>
+      <Head>
+        <title>Miyu 3D Model</title>
+      </Head>
+      <div className="h-screen w-screen bg-gray-900">
+        <Canvas camera={{ position: [0, 1, 1], fov: 60 }}>
+          <ambientLight />
+          <directionalLight position={[5, 5, 5]} intensity={1} />
+          <MiyuModel />
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </>
+  )
+}
